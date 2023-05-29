1ec3475800ff5bc03870684515cf44328953c8ef
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-05-29 17:38:20 +0500

    ewrf

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='Menu'>
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Menu
