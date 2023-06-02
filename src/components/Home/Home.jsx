594380fc7f2713d6b741ff010722d1a567418630
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-06-02 13:29:42 +0500

    e

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Home.scss'
 import logo from '../../img/Logo.svg'
+import search from '../../img/search.png'
+import person from '../../img/person.png'
+import shop from '../../img/shop.png'
+import heart from '../../img/heart.png'
 
 function Home() {
   return (
@@ -23,10 +27,12 @@ function Home() {
           </ul>
           <img className='header__img' src={logo} alt="" />
           <div className="icons">
-            <input type="text" name="" id="" />
-            <button></button>
-            <button></button>
-            <button></button>
+            <input placeholder='Search...' type="text" name="" id="" />
+ <img src={search} alt="" />
+ <img src={person} alt="" />
+ <img src={shop} alt="" />
+ <img src={heart} alt="" />
+       
           </div>
         </header>
       </div>
