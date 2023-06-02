0807b64f497cf7ba44843996a570625a95881564
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-06-02 11:15:28 +0500

    srdgw4t

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Home.scss'
+import logo from '../../img/Logo.svg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="container">
+        <header>
+          <ul className='header__list'>
+           <li className='header__list__item'>
+            Home
+           </li>
+           <li className='header__list__item'>
+           Clothes
+           </li>
+           <li className='header__list__item'>
+           Shoes
+           </li>
+           <li className='header__list__item'>
+           Accessories
+           </li>
+          </ul>
+          <img className='header__img' src={logo} alt="" />
+          <div className="icons">
+            <input type="text" name="" id="" />
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
+        </header>
+      </div>
+    </div>
   )
 }
 
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../components/Home/Home'
+
+function Main() {
+  return (
+    <div>
+<Home/>
+    </div>
+  )
+}
+
+
+export default Main
