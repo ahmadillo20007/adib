db6b50c2bbf4e8140581d7e34e76ddd96c9b30a1
faryozbek1337 <faryozbekrazzakov@gmail.com>
2023-06-02 13:52:25 +0500

    j

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './Login.scss'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='Login'>
+      <div className="container">
+        <h2 className='Login__title1'>Hello there!</h2>
+      </div>
+    </div>
   )
 }
 
