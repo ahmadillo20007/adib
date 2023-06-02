d70ff576347f091150016ab0c19d335e4d86d997
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-06-02 10:08:37 +0500

    dfh

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login';
-import SignIn from './components/SignIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
-import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Login/>}>
-          <Route path='' element={<SignUp/>}/>
-          <Route path='/signin' element={<SignIn/>}/>
-        </Route>
-        <Route path='/home' element={<Home/>}/>
-      </Routes>
+  
     </div>
   );
 }
