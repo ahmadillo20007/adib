a980db7435a720151915cc7de7fb8cb742796101
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-06-05 17:29:06 +0500

    ewf

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react'
 import './Login.scss'
-import { Link } from 'react-router-dom'
+import { Link , useNavigate} from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 
 function Login() {
+  const [data,setData] = useState([])
+  const navigate = useNavigate()
+  useEffect(()=>{
+    fetch('https://647092d63de51400f7248a57.mockapi.io/login')
+    .then((res)=>res.json())
+    .then((data)=> setData(data))
+  })
+  const handlerSignIn = (e) =>{
+    e.preventDefault()
+    let newUser = {
+        login: e.target.login.value,
+        password: e.target.password.value
+    }
+if(    data.find((item) =>item.login == newUser.login && item.password == newUser.password)) {
+navigate('/')
+}
+}
   return (
     <div className='container'>
     <Header/>
@@ -16,15 +33,16 @@ function Login() {
           <div className="Login__left">
             <h3 className='Login__left__title1'>SIGN IN</h3>
             <p className='Login__left__title2'>Email</p>
-            <input className='Login__left__inp' type="email" />
+            <input name='login' className='Login__left__inp' type="email" />
             <p className='Login__left__title3'>Password</p>
-            <input className='Login__left__inp' type="password" /><br /><br />
+            <input name='passoword' className='Login__left__inp' type="password" /><br /><br />
             <label for="check1">
             <input type="checkbox" id='check1' />
             Remeber my details
         </label> <br /><br />
-            <button className='Login__left__btn'>SIGN IN</button><br /><br />
+            <button onClick={handlerSignIn} className='Login__left__btn'>SIGN IN</button><br /><br />
             <Link className='Login__left__link'>Forgot password?</Link>
+
           </div>
           <div className="Login__right">
             <h3 className='Login__right__title1'>CREATE ACCOUNT</h3>
